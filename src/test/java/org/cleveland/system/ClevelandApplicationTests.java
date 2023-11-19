9e61303fb8fbcf88624e68ea8c912940eb21609f
chiennx <noreply.saobacdau@gmail.com>
2023-11-19 22:28:21 +0700

    Booking docter

---
+++ src/test/java/org/cleveland/system/ClevelandApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cleveland.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClevelandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
